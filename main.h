220cac9cb841e6b05ff8237cdafb722f9419e023
Hazem Ali <hazemaligalal@outlook.com>
2023-07-25 16:53:31 -0700

    amso

--- main.h
+++ main.h
@@ -7,6 +7,8 @@
 #include <unistd.h>
 #define UNUSED(x) (void)(x)
 #define BUFF_SIZE 1024
+#define S_LONG 2
+#define S_SHORT 1
 
 int _printf(const char *format, ...);
 int printmystrwrite(char str[]);
@@ -16,6 +18,12 @@ int print_int(va_list types, char buffer[],
 int my_binary(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 char convertdigit(int x);
+int we_port(char);
+int our_hexa_code(char, char[], int);
+int num_digit(char);
+
+long int size_num(long int num, int size);
+long int size_unsgnd(unsigned long int num, int size);
 int mynumlen(int num1);
 char *convertnumbertocharz(int number1);
 void print_number(int n);
