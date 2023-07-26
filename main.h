703c623bf86f706ed4dae48228dd32757ea50114
Hazem Ali <hazemaligalal@outlook.com>
2023-07-25 17:35:08 -0700

    amso

--- main.h
+++ main.h
@@ -15,17 +15,7 @@ int printmystrwrite(char str[]);
 int printmycharwrite(char charro);
 int print_int(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
-int my_binary(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
 char convertdigit(int x);
-int printROT13(va_list args);
-int we_port(char);
-int our_hexa_code(char, char[], int);
-int num_digit(char);
-int printhexadici(va_list args);
-
-long int size_num(long int num, int size);
-long int size_unsgnd(unsigned long int num, int size);
 int mynumlen(int num1);
 char *convertnumbertocharz(int number1);
 void print_number(int n);
