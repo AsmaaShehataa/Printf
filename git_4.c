b44d4cf3481924c7410c3c106216ae71aa48c8fb
AsmaaShehataa <asmaa_shihata@yahoo.com>
2023-07-26 11:06:07 +0300

    Handling betty for task-4

--- git_4.c
+++ git_4.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 
 /**
- * print_int - writes the character c to stdout
+ * myprint - writes the character c to stdout
  * @c: The character to print
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
