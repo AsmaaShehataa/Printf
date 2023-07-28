2a2d0b3eac6bb048cbf2192313f4d9067ae28535
AsmaaShehataa <asmaa_shihata@yahoo.com>
2023-07-29 01:03:02 +0300

    aa

--- testprintno.c
+++ testprintno.c
@@ -3,8 +3,9 @@
 /**
  * print_number - prints an integer
  * @n: integer to be printed
+ * Return: the integer
  */
-void print_number(int n)
+int print_number(int n)
 {
 	unsigned int num;
 
@@ -23,4 +24,51 @@ void print_number(int n)
 		print_number(num / 10);
 	}
 	printmycharwrite((num % 10) + '0');
+	return (mynumlen(n));
+}
+
+/**
+ * print_binary - prints number in binary
+ * @n: integer to be printed
+ * Return: the binary number
+ */
+int print_binary(int n)
+{
+	unsigned int num;
+
+	if (n < 0)
+	{
+		num = -n;
+		printmycharwrite('-');
+	}
+	else
+	{
+		num = n;
+	}
+	if (num / 2)
+	{
+		print_binary(num / 2);
+	}
+	printmycharwrite((num % 2) + '0');
+	return (mynumlen(n));
+}
+/**
+ * my_bin_len - prints the len of the bin number
+ * @num1: integer to be printed
+ * Return: the len binary number
+ */
+
+int my_bin_len(int num1)
+{
+	int count = 0;
+
+	if (num1 < 0)
+		count++; /* Handle - sign */
+
+	do {
+		num1 = num1 / 2;
+		count++;
+	} while (num1 != 0);
+	return (count);
+
 }
