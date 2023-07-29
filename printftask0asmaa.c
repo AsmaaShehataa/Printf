217294ec6ec32dd4b96b15c0f421153e72b37cc0
AsmaaShehataa <asmaa_shihata@yahoo.com>
2023-07-29 03:42:59 +0300

    Fixed %b functions

--- printftask0asmaa.c
+++ printftask0asmaa.c
@@ -46,9 +46,7 @@ int _printf(const char *format, ...)
 {
 	va_list args;
 
-	int i, num;
-	int counteras = 0;
-
+	int counteras = 0, i = 0;
 
 	if (!format || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
@@ -58,10 +56,12 @@ int _printf(const char *format, ...)
 	{
 		if (format[i] == '%' && (format[i + 1] == 'd' || (format[i + 1] == 'i')))
 		{
-			num = va_arg(args, int);
-			print_number(num);
-			counteras += mynumlen(num);
-
+			counteras += print_number(va_arg(args, int));
+			i = i + 1;
+		}
+		else if (format[i] == '%' && (format[i + 1] == 'b'))
+		{
+			counteras += print_binary(va_arg(args, unsigned int));
 			i = i + 1;
 		}
 		else if (format[i] == '%' && (format[i + 1] == 's'))
